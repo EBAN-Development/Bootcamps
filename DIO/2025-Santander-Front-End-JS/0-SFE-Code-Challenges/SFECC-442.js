aabd02307181fb5d74b8bf680d913a2907d3b3e1
srtazuzza <monicavc@ymail.com>
2025-06-19 13:23:28 -0300

    add Contador de Cartas de Duelista

---
+++ DIO/2025-Santander-Front-End-JS/0-SFE-Code-Challenges/SFECC-442.js
@@ -0,0 +1,30 @@
+// Desenvolvendo Soluções com JavaScript
+// 2 / 2 - Contador de Cartas de Duelista
+
+// Lendo a entrada e transformando em um array
+let cartas = gets().split(", ").map(c => c.trim());
+
+let contagem = {};
+
+// Contando as ocorrências das cartas
+cartas.forEach(
+    carta => {
+        if (contagem[carta]) {
+            contagem[carta]++;
+        } 
+        else {
+            contagem[carta] = 1;
+        }
+    }
+);
+
+// TODO: Agora, ordene as cartas alfabeticamente e exiba a saída no formato "Carta: Quantidade"
+let ordenarCartas = Object.keys(contagem).sort()
+let catalogo = ordenarCartas.reduce( (carta,quantidade) => { carta[quantidade] = contagem[quantidade]; return carta;}, {} );
+
+let textoformatado
+
+for (let carta in catalogo) { 
+    textoformatado = `${carta}: ${catalogo[carta]}`
+    console.log(textoformatado);
+}
