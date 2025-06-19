abbde76856ab9684f91bf34e56abe7307e3b0888
srtazuzza <monicavc@ymail.com>
2025-06-18 23:57:10 -0300

    add Classificação de Personagens

---
+++ DIO/2025-Santander-Front-End-JS/0-SFE-Code-Challenges/SFECC-421.js
@@ -0,0 +1,32 @@
+// Explorando Lógica de Programação com JavaScript
+// 1 / 2 - Classificação de Personagens
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+// Lê os valores de entrada e separa corretamente
+let valores = gets().split(" ");  
+let forca = parseInt(valores[0]); 
+let inteligencia = parseInt(valores[1]); 
+
+let classificacao = "";
+
+// TODO: Crie condições if/else para validação da classificação do personagem:
+if (forca < 20) {
+    if (inteligencia < 15) { 
+        classificacao = "Aprendiz"; 
+        
+    }else { 
+        classificacao = "Mago"; 
+    }
+}
+else {    
+    if (inteligencia < 15) { 
+        classificacao = "Guerreiro"; 
+        
+    }else { 
+        classificacao = "Arquimago";
+    }
+}
+// Exibe o resultado
+print(classificacao.trim());
